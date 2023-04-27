75c551ac2333016e675718d48bfbd64ca220a95d
David Camilo Sánchez Mora <david.sanchezm@pragma.com.co>
2023-04-27 02:09:12 -0500

    Remove unnecessary imports

--- src/main/java/com/pragma/powerup/usermicroservice/configuration/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/pragma/powerup/usermicroservice/configuration/security/jwt/JwtTokenFilter.java
@@ -6,8 +6,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,9 +19,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class JwtTokenFilter extends OncePerRequestFilter {
-
-    private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
-
     @Autowired
     JwtProvider jwtProvider;
 
